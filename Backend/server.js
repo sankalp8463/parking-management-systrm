0418d74027c532b071dc406e5fe768cdaf2d7e55
sankalp <sankalp8463@gmail.com>
2025-09-14 15:38:18 +0530

    done

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:3000",
+      "http://localhost:4200",
       "http://127.0.0.1:3000",
       "http://0.0.0.0:3000"
     ],
